60517cf4abdde614a2de18a1901990be3f1eabd6
bialy1994 <michal94b@gmail.com>
2016-12-12 15:02:08 +0100

    Wczytywanie plików video

--- Forms/MainForm.Designer.cs
+++ Forms/MainForm.Designer.cs
@@ -1,12 +1,15 @@
-﻿using FaceDetection.Sources;
+﻿using System.Windows.Forms;
+using Emgu.CV;
 
 namespace FaceDetection
 {
 	partial class MainForm
 	{
-		private string fileName;
-		private const int pictureWidth = 320;
-		private const int pictureHeight = 240;
+		private Capture _capture = null;
+		private bool _captureInProgress;
+		private bool _detect;
+		private int _selectedAlgorithmIndex;
+		private double _frameRate;
 		private System.ComponentModel.IContainer components = null;
 		protected override void Dispose(bool disposing)
 		{
@@ -24,17 +27,18 @@ namespace FaceDetection
 			this.components = new System.ComponentModel.Container();
 			this.loadFileCheckBox = new System.Windows.Forms.CheckBox();
 			this.webcamCheckBox = new System.Windows.Forms.CheckBox();
-			this.openFileButton = new System.Windows.Forms.Button();
 			this.detectButton = new System.Windows.Forms.Button();
-			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
 			this.pictureBox = new Emgu.CV.UI.ImageBox();
-			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.eyesCheckBox = new System.Windows.Forms.CheckBox();
 			this.noseCheckBox = new System.Windows.Forms.CheckBox();
 			this.mouthCheckBox = new System.Windows.Forms.CheckBox();
 			this.algorithmComboBox = new System.Windows.Forms.ComboBox();
+			this.openFileButton = new System.Windows.Forms.Button();
+			this.saveFileButton = new System.Windows.Forms.Button();
+			this.openImageCheckBox = new System.Windows.Forms.CheckBox();
+			this.openVideoCheckBox = new System.Windows.Forms.CheckBox();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -43,7 +47,7 @@ namespace FaceDetection
 			this.loadFileCheckBox.AutoSize = true;
 			this.loadFileCheckBox.Checked = true;
 			this.loadFileCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
-			this.loadFileCheckBox.Location = new System.Drawing.Point(12, 382);
+			this.loadFileCheckBox.Location = new System.Drawing.Point(661, 297);
 			this.loadFileCheckBox.Name = "loadFileCheckBox";
 			this.loadFileCheckBox.Size = new System.Drawing.Size(89, 17);
 			this.loadFileCheckBox.TabIndex = 1;
@@ -54,7 +58,7 @@ namespace FaceDetection
 			// webcamCheckBox
 			// 
 			this.webcamCheckBox.AutoSize = true;
-			this.webcamCheckBox.Location = new System.Drawing.Point(12, 411);
+			this.webcamCheckBox.Location = new System.Drawing.Point(661, 326);
 			this.webcamCheckBox.Name = "webcamCheckBox";
 			this.webcamCheckBox.Size = new System.Drawing.Size(116, 17);
 			this.webcamCheckBox.TabIndex = 2;
@@ -62,19 +66,9 @@ namespace FaceDetection
 			this.webcamCheckBox.UseVisualStyleBackColor = true;
 			this.webcamCheckBox.CheckedChanged += new System.EventHandler(this.webcamCheckBox_CheckedChanged);
 			// 
-			// openFileButton
-			// 
-			this.openFileButton.Location = new System.Drawing.Point(134, 378);
-			this.openFileButton.Name = "openFileButton";
-			this.openFileButton.Size = new System.Drawing.Size(107, 23);
-			this.openFileButton.TabIndex = 3;
-			this.openFileButton.Text = "Open file";
-			this.openFileButton.UseVisualStyleBackColor = true;
-			this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
-			// 
 			// detectButton
 			// 
-			this.detectButton.Location = new System.Drawing.Point(562, 378);
+			this.detectButton.Location = new System.Drawing.Point(658, 349);
 			this.detectButton.Name = "detectButton";
 			this.detectButton.Size = new System.Drawing.Size(90, 23);
 			this.detectButton.TabIndex = 5;
@@ -82,12 +76,9 @@ namespace FaceDetection
 			this.detectButton.UseVisualStyleBackColor = true;
 			this.detectButton.Click += new System.EventHandler(this.detectButton_Click);
 			// 
-			// openFileDialog
-			// 
-			this.openFileDialog.FileName = "openFileDialog1";
-			// 
 			// pictureBox
 			// 
+			this.pictureBox.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
 			this.pictureBox.Location = new System.Drawing.Point(12, 12);
 			this.pictureBox.Name = "pictureBox";
 			this.pictureBox.Size = new System.Drawing.Size(640, 360);
@@ -98,7 +89,7 @@ namespace FaceDetection
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(658, 12);
+			this.label1.Location = new System.Drawing.Point(655, 12);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(55, 13);
 			this.label1.TabIndex = 7;
@@ -107,7 +98,7 @@ namespace FaceDetection
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(658, 52);
+			this.label2.Location = new System.Drawing.Point(655, 52);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(105, 13);
 			this.label2.TabIndex = 10;
@@ -116,7 +107,7 @@ namespace FaceDetection
 			// eyesCheckBox
 			// 
 			this.eyesCheckBox.AutoSize = true;
-			this.eyesCheckBox.Location = new System.Drawing.Point(661, 68);
+			this.eyesCheckBox.Location = new System.Drawing.Point(658, 68);
 			this.eyesCheckBox.Name = "eyesCheckBox";
 			this.eyesCheckBox.Size = new System.Drawing.Size(49, 17);
 			this.eyesCheckBox.TabIndex = 11;
@@ -126,7 +117,7 @@ namespace FaceDetection
 			// noseCheckBox
 			// 
 			this.noseCheckBox.AutoSize = true;
-			this.noseCheckBox.Location = new System.Drawing.Point(661, 91);
+			this.noseCheckBox.Location = new System.Drawing.Point(658, 91);
 			this.noseCheckBox.Name = "noseCheckBox";
 			this.noseCheckBox.Size = new System.Drawing.Size(51, 17);
 			this.noseCheckBox.TabIndex = 12;
@@ -136,7 +127,7 @@ namespace FaceDetection
 			// mouthCheckBox
 			// 
 			this.mouthCheckBox.AutoSize = true;
-			this.mouthCheckBox.Location = new System.Drawing.Point(661, 114);
+			this.mouthCheckBox.Location = new System.Drawing.Point(658, 114);
 			this.mouthCheckBox.Name = "mouthCheckBox";
 			this.mouthCheckBox.Size = new System.Drawing.Size(56, 17);
 			this.mouthCheckBox.TabIndex = 13;
@@ -148,23 +139,72 @@ namespace FaceDetection
 			this.algorithmComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.algorithmComboBox.FormattingEnabled = true;
 			this.algorithmComboBox.Items.AddRange(new object[] {
-				"Frontal face - default",
-				"Frontal face - Alt",
-				"Frontal face - Alt 2",
-				"Frontal face - Alt tree",
-				"Profile face"});
-			this.algorithmComboBox.Location = new System.Drawing.Point(661, 28);
+            "Frontal face - default",
+            "Frontal face - Alt",
+            "Frontal face - Alt 2",
+            "Frontal face - Alt tree",
+            "Profile face"});
+			this.algorithmComboBox.Location = new System.Drawing.Point(658, 28);
 			this.algorithmComboBox.Name = "algorithmComboBox";
 			this.algorithmComboBox.Size = new System.Drawing.Size(121, 21);
 			this.algorithmComboBox.TabIndex = 14;
-			this.algorithmComboBox.SelectedIndexChanged += new System.EventHandler(this.algorithmComboBox_SelectedIndexChanged);
 			algorithmComboBox.SelectedIndex = 0;
+			this.algorithmComboBox.SelectedIndexChanged += new System.EventHandler(this.algorithmComboBox_SelectedIndexChanged);
+			// 
+			// openFileButton
+			// 
+			this.openFileButton.Location = new System.Drawing.Point(658, 216);
+			this.openFileButton.Name = "openFileButton";
+			this.openFileButton.Size = new System.Drawing.Size(90, 23);
+			this.openFileButton.TabIndex = 15;
+			this.openFileButton.Text = "Open File";
+			this.openFileButton.UseVisualStyleBackColor = true;
+			this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+			// 
+			// saveFileButton
+			// 
+			this.saveFileButton.Enabled = false;
+			this.saveFileButton.Location = new System.Drawing.Point(658, 268);
+			this.saveFileButton.Name = "saveFileButton";
+			this.saveFileButton.Size = new System.Drawing.Size(90, 23);
+			this.saveFileButton.TabIndex = 16;
+			this.saveFileButton.Text = "Save file";
+			this.saveFileButton.UseVisualStyleBackColor = true;
+			this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+			// 
+			// openImageCheckBox
+			// 
+			this.openImageCheckBox.AutoSize = true;
+			this.openImageCheckBox.Checked = true;
+			this.openImageCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.openImageCheckBox.Location = new System.Drawing.Point(658, 245);
+			this.openImageCheckBox.Name = "openImageCheckBox";
+			this.openImageCheckBox.Size = new System.Drawing.Size(55, 17);
+			this.openImageCheckBox.TabIndex = 17;
+			this.openImageCheckBox.Text = "Image";
+			this.openImageCheckBox.UseVisualStyleBackColor = true;
+			this.openImageCheckBox.CheckedChanged += new System.EventHandler(this.openImageCheckBox_CheckedChanged);
+			// 
+			// openVideoCheckBox
+			// 
+			this.openVideoCheckBox.AutoSize = true;
+			this.openVideoCheckBox.Location = new System.Drawing.Point(719, 245);
+			this.openVideoCheckBox.Name = "openVideoCheckBox";
+			this.openVideoCheckBox.Size = new System.Drawing.Size(53, 17);
+			this.openVideoCheckBox.TabIndex = 18;
+			this.openVideoCheckBox.Text = "Video";
+			this.openVideoCheckBox.UseVisualStyleBackColor = true;
+			this.openVideoCheckBox.CheckedChanged += new System.EventHandler(this.openVideoCheckBox_CheckedChanged);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(791, 449);
+			this.ClientSize = new System.Drawing.Size(791, 385);
+			this.Controls.Add(this.openVideoCheckBox);
+			this.Controls.Add(this.openImageCheckBox);
+			this.Controls.Add(this.saveFileButton);
+			this.Controls.Add(this.openFileButton);
 			this.Controls.Add(this.algorithmComboBox);
 			this.Controls.Add(this.mouthCheckBox);
 			this.Controls.Add(this.noseCheckBox);
@@ -173,11 +213,10 @@ namespace FaceDetection
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox);
 			this.Controls.Add(this.detectButton);
-			this.Controls.Add(this.openFileButton);
 			this.Controls.Add(this.webcamCheckBox);
 			this.Controls.Add(this.loadFileCheckBox);
 			this.Name = "MainForm";
-			this.Text = "MainForm";
+			this.Text = "Face detection";
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -185,18 +224,19 @@ namespace FaceDetection
 		}
 
 		#endregion
-		private System.Windows.Forms.CheckBox loadFileCheckBox;
-		private System.Windows.Forms.CheckBox webcamCheckBox;
-		private System.Windows.Forms.Button openFileButton;
-		private System.Windows.Forms.Button detectButton;
-		private System.Windows.Forms.OpenFileDialog openFileDialog;
+		private CheckBox loadFileCheckBox;
+		private CheckBox webcamCheckBox;
+		private Button detectButton;
 		private Emgu.CV.UI.ImageBox pictureBox;
-		private System.Windows.Forms.SaveFileDialog saveFileDialog;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.CheckBox eyesCheckBox;
-		private System.Windows.Forms.CheckBox noseCheckBox;
-		private System.Windows.Forms.CheckBox mouthCheckBox;
-		private System.Windows.Forms.ComboBox algorithmComboBox;
+		private Label label1;
+		private Label label2;
+		private CheckBox eyesCheckBox;
+		private CheckBox noseCheckBox;
+		private CheckBox mouthCheckBox;
+		private ComboBox algorithmComboBox;
+		private Button openFileButton;
+		private Button saveFileButton;
+		private CheckBox openImageCheckBox;
+		private CheckBox openVideoCheckBox;
 	}
 }
--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
@@ -8,32 +9,34 @@ namespace FaceDetection
 {
 	public partial class MainForm : Form
 	{
-		private readonly Capture _capture = null;
-		private bool _captureInProgress;
-		private bool _detect;
-		private int selectedAlgorithmIndex;
+		
 
 		public MainForm()
 		{
 			InitializeComponent();
 			CvInvoke.UseOpenCL = false;
-			try
-			{
-				_capture = new Capture();
-				_capture.ImageGrabbed += ProcessFrame;
-			}
-			catch (NullReferenceException excpt)
-			{
-				MessageBox.Show(excpt.Message);
-			}
-			selectedAlgorithmIndex = algorithmComboBox.SelectedIndex;
 		}
 
 		private void ProcessFrame(object sender, EventArgs arg)
 		{
-			var frame = new Mat();
-			_capture.Retrieve(frame, 0);
-			pictureBox.Image = _detect ? DetectFace.Detect(frame, selectedAlgorithmIndex, eyesCheckBox.Checked, mouthCheckBox.Checked, noseCheckBox.Checked) : frame;
+			if (webcamCheckBox.Checked)
+			{
+				var frame = new Mat();
+				_capture.Retrieve(frame, 0);
+				pictureBox.Image = _detect
+					? DetectFace.Detect(frame, _selectedAlgorithmIndex, eyesCheckBox.Checked, mouthCheckBox.Checked,
+						noseCheckBox.Checked)
+					: frame;
+			}else if (loadFileCheckBox.Checked)
+			{
+				var frame = _capture.QueryFrame();
+				pictureBox.Image = _detect
+					? DetectFace.Detect(frame, _selectedAlgorithmIndex, eyesCheckBox.Checked, mouthCheckBox.Checked,
+						noseCheckBox.Checked)
+					: frame;
+				Thread.Sleep((int) (1000.0/_frameRate));
+			}
+			GC.Collect();
 		}
 
 
@@ -43,6 +46,8 @@ namespace FaceDetection
 			{
 				webcamCheckBox.Checked = false;
 				openFileButton.Enabled = true;
+				openVideoCheckBox.Enabled = true;
+				openImageCheckBox.Enabled = true;
 				detectButton.Text = "detect";
 				_detect = false;
 			}
@@ -56,8 +61,20 @@ namespace FaceDetection
 		{
 			if (webcamCheckBox.Checked)
 			{
+				try
+				{
+					_capture = new Capture();
+					_capture.ImageGrabbed += ProcessFrame;
+				}
+				catch (NullReferenceException excpt)
+				{
+					MessageBox.Show(excpt.Message);
+				}
 				loadFileCheckBox.Checked = false;
 				openFileButton.Enabled = false;
+				saveFileButton.Enabled = false;
+				openVideoCheckBox.Enabled = false;
+				openImageCheckBox.Enabled = false;
 				detectButton.Text = "Start detection";
 				_detect = false;
 			}
@@ -77,25 +94,14 @@ namespace FaceDetection
 			}
 		}
 
-		private void openFileButton_Click(object sender, EventArgs e)
-		{
-			openFileDialog.Filter = "JPEG|*.jpg|PNG|*.png|BMP|*.bmp|MP4|*.mp4";
-			if (openFileDialog.ShowDialog() == DialogResult.OK)
-			{
-				var image = new Mat(openFileDialog.FileName, LoadImageType.Color);
-				pictureBox.Image = image;
-				fileName = openFileDialog.FileName;
-			}
-		}
-
 		private void detectButton_Click(object sender, EventArgs e)
 		{
 			if (pictureBox.Image == null)
 				return;
 
-			if (loadFileCheckBox.Checked)
+			if (loadFileCheckBox.Checked && openImageCheckBox.Checked)
 			{
-				pictureBox.Image = DetectFace.Detect((Mat)pictureBox.Image, selectedAlgorithmIndex, eyesCheckBox.Checked,mouthCheckBox.Checked,noseCheckBox.Checked);
+				pictureBox.Image = DetectFace.Detect((Mat)pictureBox.Image, _selectedAlgorithmIndex, eyesCheckBox.Checked,mouthCheckBox.Checked,noseCheckBox.Checked);
 				return;
 			}
 
@@ -106,7 +112,75 @@ namespace FaceDetection
 
 		private void algorithmComboBox_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			selectedAlgorithmIndex = algorithmComboBox.SelectedIndex;
+			_selectedAlgorithmIndex = algorithmComboBox.SelectedIndex;
+		}
+
+		private void openFileButton_Click(object sender, EventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog();
+			if (openImageCheckBox.Checked)
+			{
+				openFileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					Application.Idle -= ProcessFrame;
+					GC.Collect();
+					var image = new Mat(openFileDialog.FileName, LoadImageType.Color);
+					pictureBox.Image = image;
+					saveFileButton.Enabled = true;
+				}
+			}
+			else if (openVideoCheckBox.Checked)
+			{
+				openFileDialog.Filter = "Video files(*.mp4, *.avi) | *.mp4; *.avi";
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					_capture = null;
+					_capture = new Capture(openFileDialog.FileName);
+					_capture.SetCaptureProperty(CapProp.FrameWidth, 640);
+					_capture.SetCaptureProperty(CapProp.FrameHeight, 360);
+					_frameRate = _capture.GetCaptureProperty(CapProp.Fps);
+					Application.Idle += ProcessFrame;
+					
+				}
+			}
+		}
+
+		private void saveFileButton_Click(object sender, EventArgs e)
+		{
+			var saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Filter = "JPG|*.jpg|JPEG|*.jpeg|JPE|*.jpe|JFIF|*.jfif|PNG|*.png";
+			saveFileDialog.ShowDialog();
+			if (!string.IsNullOrEmpty(saveFileDialog.FileName))
+			{
+				pictureBox.Image.Save(saveFileDialog.FileName);
+			}
+		}
+
+		private void openImageCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (!openImageCheckBox.Checked && !openVideoCheckBox.Checked)
+			{
+				openFileButton.Enabled = false;
+			}
+			if (openImageCheckBox.Checked)
+			{
+				openVideoCheckBox.Checked = false;
+				openFileButton.Enabled = true;
+			}
+		}
+
+		private void openVideoCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			if (!openImageCheckBox.Checked && !openVideoCheckBox.Checked)
+			{
+				openFileButton.Enabled = false;
+			}
+			if (openVideoCheckBox.Checked)
+			{
+				openImageCheckBox.Checked = false;
+				openFileButton.Enabled = true;
+			}
 		}
 	}
 }
